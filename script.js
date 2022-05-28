f919a8b9b85711a68cd00871056bf6a8599fd11e
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-05-28 18:55:01 -0300

    Desafio 7

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const paletteColors = document.querySelectorAll ('.color');
+function selectColor() {
+   for (let index = 0; index < paletteColors.length; index +=1) {
+       paletteColors[index].addEventListener('click', () => {
+           const selectedHolder = document.querySelector('.selected');
+           selectedHolder.classList.remove('selected');
+           paletteColors[index].classList.add('selected')
+       })
+       
+   } 
+}
+selectColor();
+
 
 
     
